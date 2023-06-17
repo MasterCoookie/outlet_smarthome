e4ac5bf47cb5f08384bbb84bb908e20f40e4954c
MasterCoookie <jas.kocurek2@wp.pl>
2023-06-17 14:52:09 +0200

    adds mqtt connection

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,47 @@ PubSubClient client(espClient);
 const char* mqtt_server = "1.tcp.eu.ngrok.io";
 short int mqtt_port = 21589;
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // deserialize json
+  StaticJsonDocument<244> doc;
+  deserializeJson(doc, payload, length);
+
+  // read values
+  short unsigned int idx = doc["idx"];
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.println("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("JK_ESP8266_0", mqtt_username, mqtt_pwd)) {
+      Serial.print("connected with ");
+      Serial.print(mqtt_username);
+      Serial.println("!");
+      // Subscribe
+      Serial.println("Subscribing to topics:");
+      Serial.println("esp/out/9");
+      client.subscribe("esp/out/9");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds...");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
 void setup() {
   Serial.begin(115200);
 
@@ -34,12 +75,14 @@ void setup() {
   // wifi connection ends
 
   // mqtt connection starts
-  // PubSubClient client(mqtt_server, 1883, callback, espClient);
-
-
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
 }
 
 void loop() {
-  Serial.println("Dupa");
-  delay(1000);
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  client.loop();
 }
